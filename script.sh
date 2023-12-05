b72d95bd2d0b20c6315c6d20980e28a39d9fac2f
Brendan Mulholland <brendan@recital.software>
2023-12-05 23:21:52 +0100

    Switch to yarn vue-tsc

--- script.sh
+++ script.sh
@@ -4,33 +4,18 @@ cd "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" || exit 1
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-if [ ! -f "$(npm root)"/.bin/tsc ]; then
-  echo "::group::🔄 Running npm install to install tsc ..."
-  npm install
-  echo "::endgroup::"
-fi
+echo "::group::📝 Running vue-tsc with reviewdog 🐶 ..."
 
-if [ ! -f "$(npm root)"/.bin/tsc ]; then
-  echo "❌ Unable to locate or install tsc. Did you provide a workdir which contains a valid package.json?"
-  exit 1
-else
+# shellcheck disable=SC2086
+yarn run vue-tsc ${INPUT_TSC_FLAGS} |
+  reviewdog -f=tsc \
+    -name="${INPUT_TOOL_NAME}" \
+    -reporter="${INPUT_REPORTER}" \
+    -filter-mode="${INPUT_FILTER_MODE}" \
+    -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
+    -level="${INPUT_LEVEL}" \
+    ${INPUT_REVIEWDOG_FLAGS}
 
-  echo ℹ️ tsc version: "$("$(npm root)"/.bin/tsc --version)"
-
-  echo "::group::📝 Running tsc with reviewdog 🐶 ..."
-
-  # shellcheck disable=SC2086
-  "$(npm root)"/.bin/tsc ${INPUT_TSC_FLAGS} |
-    reviewdog -f=tsc \
-      -name="${INPUT_TOOL_NAME}" \
-      -reporter="${INPUT_REPORTER}" \
-      -filter-mode="${INPUT_FILTER_MODE}" \
-      -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
-      -level="${INPUT_LEVEL}" \
-      ${INPUT_REVIEWDOG_FLAGS}
-
-  reviewdog_rc=$?
-  echo "::endgroup::"
-  exit $reviewdog_rc
-
-fi
+reviewdog_rc=$?
+echo "::endgroup::"
+exit $reviewdog_rc
